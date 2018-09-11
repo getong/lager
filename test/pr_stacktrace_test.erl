e758e2f56e392fbbcbc50eb3baeb7dab6baa09e5
getong <3949379+getong@users.noreply.github.com>
2018-09-11 22:57:26 +0800

    erlang/otp deprecated erlang:get_stacktrace/0 function, use the function as platform version detected

--- test/pr_stacktrace_test.erl
+++ test/pr_stacktrace_test.erl
@@ -2,6 +2,14 @@
 
 -compile([{parse_transform, lager_transform}]).
 
+-ifdef(OTP_RELEASE). %% this implies 21 or higher
+-define(EXCEPTION(Class, Reason, Stacktrace), Class:Reason:Stacktrace).
+-define(GET_STACK(Stacktrace), Stacktrace).
+-else.
+-define(EXCEPTION(Class, Reason, _), Class:Reason).
+-define(GET_STACK(_), erlang:get_stacktrace()).
+-endif.
+
 -include_lib("eunit/include/eunit.hrl").
 
 make_throw() ->
@@ -17,8 +25,8 @@ pr_stacktrace_throw_test() ->
     Result = try
         make_throw()
     catch
-        Class:Reason ->
-            lager:pr_stacktrace(erlang:get_stacktrace(), {Class, Reason})
+        ?EXCEPTION(Class, Reason, Stacktrace) ->
+            lager:pr_stacktrace(?GET_STACK(Stacktrace), {Class, Reason})
     end,
 ExpectedPart = "
     pr_stacktrace_test:pr_stacktrace_throw_test/0 line 18
@@ -31,8 +39,8 @@ pr_stacktrace_bad_arg_test() ->
     Result = try
         bad_arg()
     catch
-        Class:Reason ->
-            lager:pr_stacktrace(erlang:get_stacktrace(), {Class, Reason})
+        ?EXCEPTION(Class, Reason, Stacktrace) ->
+            lager:pr_stacktrace(?GET_STACK(Stacktrace), {Class, Reason})
     end,
 ExpectedPart = "
     pr_stacktrace_test:pr_stacktrace_bad_arg_test/0 line 32
@@ -45,8 +53,8 @@ pr_stacktrace_bad_arity_test() ->
     Result = try
         bad_arity()
     catch
-        Class:Reason ->
-            lager:pr_stacktrace(erlang:get_stacktrace(), {Class, Reason})
+        ?EXCEPTION(Class, Reason, Stacktrace) ->
+            lager:pr_stacktrace(?GET_STACK(Stacktrace), {Class, Reason})
     end,
 ExpectedPart = "
     pr_stacktrace_test:pr_stacktrace_bad_arity_test/0 line 46
@@ -59,8 +67,8 @@ pr_stacktrace_no_reverse_test() ->
     Result = try
         bad_arity()
     catch
-        Class:Reason ->
-            lager:pr_stacktrace(erlang:get_stacktrace(), {Class, Reason})
+        ?EXCEPTION(Class, Reason, Stacktrace) ->
+            lager:pr_stacktrace(?GET_STACK(Stacktrace), {Class, Reason})
     end,
 ExpectedPart = "error:undef
     lists:concat([], [])
